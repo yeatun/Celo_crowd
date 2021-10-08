eb69d4e4ed59b65404a9696e5dfa132ec20d6a95
Tahlil <bsse0803@iit.du.ac.bd>
2021-10-08 20:37:50 +0600

    update the contract to allow slashing

--- smart-contract/Contracts/Campaigns.sol
+++ smart-contract/Contracts/Campaigns.sol
@@ -25,7 +25,12 @@ contract Campaign {
       mapping(address => bool) approvals;
   }
 
+    
+  mapping(address => uint) stakerAmount;
+  
+
   Request[] public requests;
+  
   address public manager;
   uint public minimunContribution;
   string public CampaignName;
@@ -37,11 +42,13 @@ contract Campaign {
   uint public CampaignDeadline;
   mapping(address => bool) public approvers;
   mapping(address => uint) public contributerAmount;
+  mapping(address => bool) public projectApprovers;
+  uint public projectApprovalCounter;
+
+  uint public numberOfStaker;
   uint public approversCount;
 
 
-
-  
 
    constructor(uint minimun, address creator,string name,string description,string image,uint target, uint deadline) public {
       manager = creator;
@@ -69,16 +76,31 @@ contract Campaign {
       _;
   }
 
-modifier notManager() {
-      require(msg.sender != manager, "Only allowed for campaign creator");
-      _;
-  }
+    modifier notManager() {
+        require(msg.sender != manager, "Only allowed for campaign creator");
+        _;
+    }
+    
+    modifier notStaker() {
+        require(stakerAmount[msg.sender] == 0, "Already a staker");
+        _;
+    }
+
+    modifier isStaker() {
+        require(stakerAmount[msg.sender] != 0, "Not a staker");
+        _;
+    }
 
     modifier isContributer() {
         require(approvers[msg.sender], "Not a contributer");
         _;
     }
 
+    modifier hasNotApprove(){
+        require(!projectApprovers[msg.sender], "Already approve");
+        _;
+    }
+   
   function contibute() public payable timeThresholdNotPast{
       require(msg.value > minimunContribution );
       contributerAmount[msg.sender] = msg.value;
@@ -108,6 +130,27 @@ modifier notManager() {
       requests[index].approvals[msg.sender] = true;
       requests[index].approvalCount++;
   }
+
+  function stake() public payable timeThresholdNotPast notManager notStaker{
+      numberOfStaker += 1;
+      stakerAmount[msg.sender] = msg.value;
+  }
+
+    function approveProject() public hasNotApprove timeThresholdPast{
+        projectApprovalCounter+=1;
+        projectApprovers[msg.sender] = true; 
+    }
+
+  function claimProjectSuccess() public payable timeThresholdPast isStaker{
+      require(projectApprovalCounter > (approversCount / 2), "No approved");
+      msg.sender.transfer(stakerAmount[msg.sender]*99/100);
+      address(this.manager).transfer(stakerAmount[msg.sender]/100);
+  }
+
+  function slash() public payable timeThresholdPast(){
+      address(0xD1520bf9ED35dda2e4DE17a88d1C2fbA273119d2).transfer(stakerAmount[msg.sender]);
+  } 
+
 
   function finalizeRequest(uint index) public restricted timeThresholdNotPast{
       require(requests[index].approvalCount > (approversCount / 2));
